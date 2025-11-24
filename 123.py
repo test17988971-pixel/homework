980612764d76cc1209351a75478d6eabdc62fcc7
test1798 <test.17988971@gmail.com>
2025-11-24 11:19:27 +0300

    last work

--- 123.py
+++ 123.py
@@ -1,3 +1,49 @@
 import pandas as pd
 data=pd.read_csv('titanic.csv')
+print(data.info())
+
+
+import pandas as pd
+data=pd.read_csv('titanic.csv')
+print(data['Sex'].value_counts())
+
+
+import pandas as pd
+data=pd.read_csv('titanic.csv')
+print(data['Survived'].value_counts())
+
+
+import pandas as pd
+data=pd.read_csv('titanic.csv')
+print(data.groupby('Sex')['Survived'].value_counts())
+
+
+import pandas as pd
+data=pd.read_csv('titanic.csv')
+print(data.groupby('Pclass')['Survived'].value_counts())
+
+
+import pandas as pd
+data=pd.read_csv('titanic.csv')
+print(data['Age'].describe())
+
+
+import pandas as pd
+data=pd.read_csv('titanic.csv')
+data['FamilySize']=data['Parch']+data['SibSp']+1
+print(data['FamilySize'][15])
+
+
+import pandas as pd
+data=pd.read_csv('titanic.csv')
+print(data.groupby('Survived')['Age'].mean())
+
+
+import pandas as pd
+data=pd.read_csv('titanic.csv')
+print(data[(data['Sex'] == 'female') & (data['Pclass'] == 1) & (data['Survived'] == 1)].count())
+
+
+import pandas as pd
+data=pd.read_csv('titanic.csv')
 print(data[(data['Age'] < 18) & (data['Parch'] == 0)].count())
